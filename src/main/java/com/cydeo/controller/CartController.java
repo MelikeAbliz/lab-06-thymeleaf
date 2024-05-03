659adb8affdb92045a08c0d3321ee32af844f3c8
MelikeAbliz <medine20171121@gmail.com>
2024-05-03 13:32:50 +0300

    cart part completed

--- src/main/java/com/cydeo/controller/CartController.java
+++ src/main/java/com/cydeo/controller/CartController.java
@@ -1,13 +1,35 @@
 package com.cydeo.controller;
 
+import com.cydeo.service.CartService;
+import com.cydeo.service.impl.CartServiceImpl;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
 
 @Controller
+@AllArgsConstructor
 public class CartController {
+    private CartService cartService;
     @GetMapping("/cart")
-    private String showCart(){
+    private String showCart(Model model){
 
+        model.addAttribute("cart", CartServiceImpl.CART);
         return "/cart/show-cart";
+    }
+    @GetMapping("/addToCart/{productId}/{quantity}")
+    public String addCart(@PathVariable("productId")UUID productId,
+                          @PathVariable("quantity")Integer quantity){
+        cartService.addToCart(productId, quantity);
+        return "redirect:/cart";
+    }
+
+    @GetMapping("/delete/{productId}")
+    public String deleteProductFromCart(@PathVariable("productId")UUID productId){
+        cartService.deleteFromCart(productId);
+        return "redirect:/cart";
     }
 }
